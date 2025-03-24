03f3829aaad3de0ff4049dabe4375ffecd4b76f3
asad <asadSuzan7@gmail.com>
2025-03-24 14:39:46 +0600

    feat(database-setup):setup database

---
+++ postgreSQL_assignment.sql
@@ -0,0 +1,9 @@
+-- Database Setup
+
+-- 1. Create a new database named "bookstore_db" or any appropriate name.
+CREATE DATABASE bookstore_db;
+
+-- 2. Connect to the newly created database.
+\c bookstore_db;
+
+-- 3. Create the "books" table with the required columns.
