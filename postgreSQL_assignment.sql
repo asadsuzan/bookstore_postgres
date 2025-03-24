7195447e77189dccd229972596244e23c654f022
asad <asadSuzan7@gmail.com>
2025-03-24 18:44:32 +0600

    sloved:(problem 3)

--- postgreSQL_assignment.sql
+++ postgreSQL_assignment.sql
@@ -61,3 +61,11 @@ SELECT * FROM books WHERE stock = 0;
 -- 2️⃣ Retrieve the most expensive book in the store.
 
 SELECT * FROM books ORDER BY price DESC LIMIT 1;
+
+-- 3️⃣ Find the total number of orders placed by each customer.
+SELECT c.name, COUNT(o.id) AS total_orders
+FROM customers c
+LEFT JOIN orders o ON c.id = o.customer_id
+GROUP BY c.name;
+
+
